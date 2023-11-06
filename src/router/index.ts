b5d9ea94393522666d41351483976c29bc020526
Jonathan <jonathan@auroracoop.com.br>
2023-11-06 08:56:32 -0300

    FIx: Auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { authService } from '@/services/authService'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
